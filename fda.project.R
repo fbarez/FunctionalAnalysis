1107ad019957bb00f19dc99ab85e22dcd214863a
Fazl Barez (Admin) <fazlbarez93@gmail.com>
2020-02-10 19:45:35 +0000

    adding fda project rm cmments

--- fda.project.R
+++ fda.project.R
@@ -327,101 +327,99 @@ plot(1:19, log10(lipeigvals[1:19]), type="b",
      xlab="Eigenvalue Number", ylab="", main="Log10 Eigenvalues")
 
 
-    # dev.off()
+    dev.off()
 
-#write.dcf(x, file = "~/Desktop/Funcational Anlaysis Project")
-# ##
-# ## 4.  Principal Differential Analysis
-# ##
-# #  ---------------------------------------------------------------------
-# #                    Principal differential analysis
-# #  ---------------------------------------------------------------------
-#
-# #  set up a second order linear differnetial equation solution
-#
-# liprange = range(liptime)
-#
-# pdabasisfd <- create.bspline.basis(liprange, nbasis=21)
-# betafdPar  <- fdPar(pdabasisfd)
-#
-# #  set up list of functional parameter objects for weight fns.
-#
-# bwtlist = vector("list", 2)
-# bwtlist[[1]] <- betafdPar
-# bwtlist[[2]] <- betafdPar
-#
-# xfdlist <- list(lipfd)
-#
-# pdaList <- pda.fd(xfdlist, bwtlist)
-#
-# #  plot weight functions
-#
-# bwtestlist <- pdaList$bwtlist
-#
-# par(mfrow=c(2,1),pty="m")
-# for (j in 1:2) {
-#   bfdParj <- bwtestlist[[j]]
-#   bvals = eval.fd(liptime,bwtestlist[[j]]$fd)
-#   plot(liptime,bvals,type='l')
-# }
-#
-# #  compute forcing functions
-#
-# Lfdest <- Lfd(2, bwtestlist)
-#
-# force        <- eval.fd(liptime, lipfd, Lfdest)
-# lipaccel     <- eval.fd(liptime, lipfd, 2)
-# lipmeanaccel <- apply(lipaccel, 1, mean)
-#
-# par(mfrow=c(1,1),ask=FALSE)
-# yrange <- c(min(min(lipmeanaccel),min(force)),
-#             max(max(lipmeanaccel),max(force)))
-# matplot(liptime, force, type="l", lty=1, ylim=yrange)
-# lines(liptime, lipmeanaccel, lty=4, lwd=2)
-#
-# #  plot the mean forcing function along with second deriv.
-#
-# forcemean <- apply(force, 1, mean)
-#
-# plot(liptime, forcemean, type="l", lty=1, ylim=yrange)
-# lines(liptime, lipmeanaccel, lty=4)
-#
-# #  solve equation
-#
-# result <- odesolv(bwtestlist)
-# xp <- result[[1]]
-# yp <- result[[2]]
-#
-# #  plot the two solutions
-#
-# par(mfrow=c(2,1),pty="m")
-# pltrng <- c(min(yp[1,,]), max(yp[1,,]))
-# matplot(xp,t(yp[1,,]), type="l", lty=1, ylim=pltrng, main="Function")
-# abline(h=0, lty=2)
-# pltrng <- c(min(yp[2,,]), max(yp[2,,]))
-# matplot(xp,t(yp[2,,]), type="l", lty=1, ylim=pltrng, main="Derivative")
-# abline(h=0, lty=2)
-#
-# #  plot fit to each curve
-#
-# lipmat   <- eval.fd(liptime, lipfd)
-# D2lipmat <- eval.fd(liptime, lipfd, 2)
-#
-# umat <- matrix(0,length(liptime),2)
-# umat[,1] <- approx(xp, t(yp[1,1,]), liptime)$y
-# umat[,2] <- approx(xp, t(yp[1,2,]), liptime)$y
-#
-# par(mfrow=c(1,2),pty="s",ask=TRUE)
-# index <- 1:20
-# for (i in index) {
-#   plot(liptime, force[,i], type="l",
-#        ylim=c(-1000,1000), xlab="Normalized Time", ylab="",
-#        main=paste("Record",i,"Forcing Fn."))
-#   lines(liptime, D2lipmat[,i],lty=4)
-#   abline(h=0,lty=2)
-#   xhat <- lipmat[,i] - lsfit(umat, lipmat[,i], int=FALSE)$residual
-#   matplot(liptime, cbind(xhat, lipmat[,i]), type="l", lty=c(1,2),
-#           xlab="Normalized Time", ylab="", main="Function")
-# }
-#
-#
+write.dcf(x, file = "~/Desktop/Funcational Anlaysis Project")
+##
+## 4.  Principal Differential Analysis
+##
+#  ---------------------------------------------------------------------
+#                    Principal differential analysis
+#  ---------------------------------------------------------------------
+
+#  set up a second order linear differnetial equation solution
+
+liprange = range(liptime)
+
+pdabasisfd <- create.bspline.basis(liprange, nbasis=21)
+betafdPar  <- fdPar(pdabasisfd)
+
+#  set up list of functional parameter objects for weight fns.
+
+bwtlist = vector("list", 2)
+bwtlist[[1]] <- betafdPar
+bwtlist[[2]] <- betafdPar
+
+xfdlist <- list(lipfd)
+
+pdaList <- pda.fd(xfdlist, bwtlist)
+
+#  plot weight functions
+
+bwtestlist <- pdaList$bwtlist
+
+par(mfrow=c(2,1),pty="m")
+for (j in 1:2) {
+  bfdParj <- bwtestlist[[j]]
+  bvals = eval.fd(liptime,bwtestlist[[j]]$fd)
+  plot(liptime,bvals,type='l')
+}
+
+#  compute forcing functions
+
+Lfdest <- Lfd(2, bwtestlist)
+
+force        <- eval.fd(liptime, lipfd, Lfdest)
+lipaccel     <- eval.fd(liptime, lipfd, 2)
+lipmeanaccel <- apply(lipaccel, 1, mean)
+
+par(mfrow=c(1,1),ask=FALSE)
+yrange <- c(min(min(lipmeanaccel),min(force)),
+            max(max(lipmeanaccel),max(force)))
+matplot(liptime, force, type="l", lty=1, ylim=yrange)
+lines(liptime, lipmeanaccel, lty=4, lwd=2)
+
+#  plot the mean forcing function along with second deriv.
+
+forcemean <- apply(force, 1, mean)
+
+plot(liptime, forcemean, type="l", lty=1, ylim=yrange)
+lines(liptime, lipmeanaccel, lty=4)
+
+#  solve equation
+
+result <- odesolv(bwtestlist)
+xp <- result[[1]]
+yp <- result[[2]]
+
+#  plot the two solutions
+
+par(mfrow=c(2,1),pty="m")
+pltrng <- c(min(yp[1,,]), max(yp[1,,]))
+matplot(xp,t(yp[1,,]), type="l", lty=1, ylim=pltrng, main="Function")
+abline(h=0, lty=2)
+pltrng <- c(min(yp[2,,]), max(yp[2,,]))
+matplot(xp,t(yp[2,,]), type="l", lty=1, ylim=pltrng, main="Derivative")
+abline(h=0, lty=2)
+
+#  plot fit to each curve
+
+lipmat   <- eval.fd(liptime, lipfd)
+D2lipmat <- eval.fd(liptime, lipfd, 2)
+
+umat <- matrix(0,length(liptime),2)
+umat[,1] <- approx(xp, t(yp[1,1,]), liptime)$y
+umat[,2] <- approx(xp, t(yp[1,2,]), liptime)$y
+
+par(mfrow=c(1,2),pty="s",ask=TRUE)
+index <- 1:20
+for (i in index) {
+  plot(liptime, force[,i], type="l",
+       ylim=c(-1000,1000), xlab="Normalized Time", ylab="",
+       main=paste("Record",i,"Forcing Fn."))
+  lines(liptime, D2lipmat[,i],lty=4)
+  abline(h=0,lty=2)
+  xhat <- lipmat[,i] - lsfit(umat, lipmat[,i], int=FALSE)$residual
+  matplot(liptime, cbind(xhat, lipmat[,i]), type="l", lty=c(1,2),
+          xlab="Normalized Time", ylab="", main="Function")
+}
